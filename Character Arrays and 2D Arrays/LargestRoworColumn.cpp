c629e349e6ad1ed05504ff32ae922f121de518dc
saksham51 <93071449+saksham51@users.noreply.github.com>
2022-05-06 11:06:54 +0530

    LargestRoworColumn.cpp

--- Character Arrays and 2D Arrays/LargestRoworColumn.cpp
+++ Character Arrays and 2D Arrays/LargestRoworColumn.cpp
@@ -23,54 +23,51 @@ column 2 25
 
 
 #include<climits>
-void findLargest(int arr[][1000], int row, int col){
-    	int i,j,sum = 0;
-	    int tempr = INT_MIN;
-	    int maxrow = 0, maxr = 0;
+void findLargest(int **arr, int row, int col){
+
+	    int largestSum = INT_MIN;
+	    int maxrow = 0;
+    	bool rowMax=true;
 	// finding each row sum 
-	for(i = 0; i < row; i++){ 
-		for (j = 0; j < col; j++) { 
+	for(int i = 0; i < row; i++){ 
+        
+        int sum = 0;
+		for (int j = 0; j < col; j++) { 
 			// Add the element 
 			sum = sum + arr[i][j]; 
 		}
         //finding th greatest row
-		if(sum > tempr){
-			maxrow = sum;
-			maxr = i;
-			tempr = maxrow;
+		if(sum > largestSum){
+            largestSum=sum;
+			maxrow = i;
 		}
-		// Reset the sum 
-		sum = 0; 
+		// Reset the sum
 	}
 	
-	i=0,j=0,sum = 0; 
-	int tempc = INT_MIN;
-	int maxcol = 0, maxc = 0;
+	
+	int maxcol = 0;
 	
 	// finding the each column sum 
-	for (i = 0; i < col; ++i) { 
-		for (j = 0; j < row; ++j) { 
+	for (int i = 0; i < col; ++i) { 
+        int sum=0;
+		for (int j = 0; j < row; ++j) { 
 
 			// Add the element 
 			sum = sum + arr[j][i]; 
 		}
         // finding th greatest column
-		if(sum > tempc){
-			maxcol = sum;
-			maxc = i;
-			tempc = maxcol;
+		if(sum > largestSum){
+			largestSum = sum;
+			maxcol = i;
+            rowMax=false;
 		}
 		// Reset the sum 
-		sum = 0;
 	}
     //comparing the max row or col
-	if(maxrow > maxcol){
-		cout<<"row " << maxr<<" "<<maxrow;
-	}
-	else if(maxrow < maxcol){
-		cout<<"column " << maxc<<" "<<maxcol;
-	}
-	else{
-		cout<<"row " << maxr<<" "<<maxrow;
-	}
+	if(rowMax){
+        cout<<"row"<<" "<<maxrow<<" "<<largestSum<<endl;
+    }
+    else{
+        cout<<"column"<<" "<<maxcol<<" "<<largestSum<<endl;
+    }
 }
